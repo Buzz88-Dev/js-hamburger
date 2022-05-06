b3a940d3150b432ffc01cb81203e0d1e27780187
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-05-06 16:27:30 +0200

    script ended

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-//
+// creo variabile clickOpen collegata al menu tendina con classe fa-bars
 let clickOpen = document.querySelector(".fa-bars");
 console.log(clickOpen);
 
+// creo variabile clickClose collegata alla "X" di chiusura con classe fa-times
 let clickClose = document.querySelector(".fa-times");
 console.log(clickClose);
 
+// creo var collegata al menu hamburger
 let menuHamburger = document.querySelector(".hamburger-menu");
 console.log(menuHamburger);
 
-
+// quando clicco su clickOpen imposto display block al menu hamburger
 clickOpen.addEventListener("click",
 
     function(){
@@ -17,6 +19,7 @@ clickOpen.addEventListener("click",
     }
 );
 
+// quando clicco su clickClose imposto display none al menu hamburger
 clickClose.addEventListener("click",
 
     function(){
